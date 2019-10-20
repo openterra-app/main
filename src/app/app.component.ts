ee30459785c5954dfdedd42a2e9fe62eb4d9e5fc
mooshgleek <rolandoaguilera99@gmail.com>
2019-10-20 03:44:43 -0300

    Development of login system based on user auth API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'openterra-main';
+  records = []
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+    
+  }
 }
